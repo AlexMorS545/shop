480b75d97494bed8ad4d4710d603d42a96162a39
Alex_MorS <morsan20@yandex.ru>
2020-11-15 02:04:03 +0500

    add html file

---
+++ src/public/index.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport"
+				content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" rel="stylesheet">
+	<link rel="stylesheet" href="css/normalize.css">
+	<link rel="stylesheet" href="css/style.css">
+	<title>My Shop</title>
+</head>
+<body>
+<header class="header">
+	<div class="header-wrp">
+		<div class="logo-wrp">
+			<div class="logo"></div>
+			<div class="logo-name">Brand</div>
+		</div>
+		<div class="menu-wrp">
+			<ul class="menu">
+				<a href="/" class="menu-link">главная</a>
+				<a href="#" class="menu-link">каталог</a>
+				<a href="#" class="menu-link">контакты</a>
+				<a href="#" class="menu-link">о нас</a>
+			</ul>
+		</div>
+		<div class="icon-wrp">
+			<i class="far fa-user"></i>
+			<i class="fas fa-shopping-basket"></i>
+		</div>
+	</div>
+</header>
+<main>
+	<div class="promo">
+		<h1 class="title">Интернет магазин</h1>
+	</div>
+
+</main>
+</body>
+</html>
+
