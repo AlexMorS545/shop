00b7afa6dae9f09c90b8647bd102c444d82cf3ee
Alex_MorS <morsan20@yandex.ru>
2021-01-16 18:15:09 +0500

    add some file

--- src/public/modules/form.php
+++ src/public/modules/form.php
@@ -5,7 +5,7 @@ include_once('server.php');
 if(isset($_POST['add'])){
   $name = trim(strip_tags($_POST['name']));
   $text = trim(strip_tags($_POST['text']));
-  $strSql = "INSERT INTO comment(name, text) VALUES ('%s', '%s')";
+  $strSql = "INSERT INTO comment (name, text) VALUES ('%s', '%s')";
   $query = sprintf($strSql, mysqli_real_escape_string($connect, $name), mysqli_real_escape_string($connect, $text));
   $res = mysqli_query($connect, $query);
 }
